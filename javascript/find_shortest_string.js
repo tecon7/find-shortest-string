468779eee964cbaf9a9d60f2d2a61d185367a404
Teddy Economou <teddyecon@gmail.com>
2023-02-15 16:32:12 -0500

    complete

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,14 @@
+// Time complexity: O(n)
+// Space complexity: O(1)
+
 function findShortestString(arr) {
-  // type your code here
+  let smallest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < smallest.length){
+      smallest = arr[i];
+    }
+  }
+  return smallest;
 }
 
 if (require.main === module) {
